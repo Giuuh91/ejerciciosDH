143c43af726393b1e49d1e089da8db75e23fe92f
Unknown <bertinigiuliano@hotmail.com>
2018-10-13 10:43:15 -0300

    Paso 3 , crear la clase Personaje

---
+++ app/src/main/java/com/example/digital/recyclerview/Personaje.java
@@ -0,0 +1,11 @@
+package com.example.digital.recyclerview;
+
+import android.widget.ImageView;
+
+public class Personaje {
+
+    private String nombre;
+    private String programa;
+    private Integer imagenPersonaje;
+    
+}
