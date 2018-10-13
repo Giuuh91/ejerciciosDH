4d2e7b86f27a2930204e742313805373fa946197
Unknown <bertinigiuliano@hotmail.com>
2018-10-13 11:09:59 -0300

    Paso 4.3 sobreescribir constructor y agregar los componentes del layout a la celda y buscarlos con el find

--- app/src/main/java/com/example/digital/recyclerview/PersonajeAdapter.java
+++ app/src/main/java/com/example/digital/recyclerview/PersonajeAdapter.java
@@ -1,10 +1,25 @@
 package com.example.digital.recyclerview;
 
+import android.media.Image;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class Adapter extends RecyclerView.Adapter {
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class PersonajeAdapter extends RecyclerView.Adapter {
+
+    private List<Personaje> listaDePersonajes;
+
+    public PersonajeAdapter(List<Personaje> listaDePersonajes) {
+        this.listaDePersonajes = listaDePersonajes;
+    }
+
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -19,5 +34,18 @@ public class Adapter extends RecyclerView.Adapter {
     @Override
     public int getItemCount() {
         return 0;
+    }
+
+    private class PersonajeViewHolder extends RecyclerView.ViewHolder {
+        private ImageView imageViewPersonaje;
+        private TextView nombrePersonaje;
+        private TextView programaPersonaje;
+
+        public PersonajeViewHolder(View itemView) {
+            super(itemView);
+           imageViewPersonaje =  itemView.findViewById(R.id.celda_imageView_personaje);
+           nombrePersonaje = itemView.findViewById(R.id.celda_textView_personaje);
+           programaPersonaje = itemView.findViewById(R.id.celda_textView_programa);
+        }
     }
 }
