c5378eaa6fb0c5e8432cba27a9f923566f697cc8
Unknown <bertinigiuliano@hotmail.com>
2018-10-13 11:44:54 -0300

    Paso4.7 sobreescribir los metodos get,oncreat,onbind

--- app/src/main/java/com/example/digital/recyclerview/PersonajeAdapter.java
+++ app/src/main/java/com/example/digital/recyclerview/PersonajeAdapter.java
@@ -1,8 +1,10 @@
 package com.example.digital.recyclerview;
 
+import android.content.Context;
 import android.media.Image;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -11,29 +13,41 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 import java.util.List;
+import java.util.zip.Inflater;
 
 public class PersonajeAdapter extends RecyclerView.Adapter {
 
     private List<Personaje> listaDePersonajes;
 
+
     public PersonajeAdapter(List<Personaje> listaDePersonajes) {
         this.listaDePersonajes = listaDePersonajes;
+
     }
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflador = LayoutInflater.from(parent.getContext());
+        View viewCelda = inflador.inflate(R.layout.celda_personaje,parent,false);
+
+        PersonajeViewHolder personajeViewHolder = new PersonajeViewHolder(viewCelda);
+
+        return personajeViewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
 
+        Personaje personaje = listaDePersonajes.get(position);
+        PersonajeViewHolder personajeViewHolder = (PersonajeViewHolder) holder;
+        personajeViewHolder.bind(personaje);
+        
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listaDePersonajes.size();
     }
 
     private class PersonajeViewHolder extends RecyclerView.ViewHolder {
@@ -51,7 +65,7 @@ public class PersonajeAdapter extends RecyclerView.Adapter {
         public void bind(Personaje unPersonaje){
             nombrePersonaje.setText(unPersonaje.getNombre());
             programaPersonaje.setText(unPersonaje.getPrograma());
-            
+
         }
     }
 }
