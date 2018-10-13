5cbe4f68be9374ad047b95e92fb919cf3711f350
Unknown <bertinigiuliano@hotmail.com>
2018-10-13 10:47:14 -0300

    Paso 4 , crear la clase adapter y sobreescribir datos

--- app/src/main/java/com/example/digital/recyclerview/Personaje.java
+++ app/src/main/java/com/example/digital/recyclerview/Personaje.java
@@ -7,5 +7,37 @@ public class Personaje {
     private String nombre;
     private String programa;
     private Integer imagenPersonaje;
-    
+
+    public Personaje() {
+    }
+
+    public Personaje(String nombre, String programa, Integer imagenPersonaje) {
+        this.nombre = nombre;
+        this.programa = programa;
+        this.imagenPersonaje = imagenPersonaje;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrograma() {
+        return programa;
+    }
+
+    public void setPrograma(String programa) {
+        this.programa = programa;
+    }
+
+    public Integer getImagenPersonaje() {
+        return imagenPersonaje;
+    }
+
+    public void setImagenPersonaje(Integer imagenPersonaje) {
+        this.imagenPersonaje = imagenPersonaje;
+    }
 }
